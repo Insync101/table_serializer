c5dc1d083839db0c49552445775f1ad7d7ea987f
Corewave <45573459+Corewave@users.noreply.github.com>
2020-08-22 17:15:04 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,65 +1,89 @@
 type = (typeof or type)
 local function compare(o, ...)
-	for i, v in next, {...} do
-		if o == v then
-			return true
-		end
-	end
+    for i, v in next, {...} do
+        if o == v then
+            return true
+        end
+    end
 
-	return false
+    return false
 end
 
-local function count_table(t)
-	local counter = 0
-	for i,v in next, t do
-		counter = counter + 1
-	end
+local function countTable(t)
+    local counter = 0
+    for i,v in next, t do
+        counter = counter + 1
+    end
 
-	return counter
+    return counter
 end
 
-local function format_value(val)
-	local data = type(val)
+local function stringRet(v)
+    local mt = (getrawmetatable or getmetatable)(v)
+    local tostring_meta, addr
 
-	if compare(data, 'number', 'boolean', 'function', 'userdata', 'table', 'nil') then
-		return tostring(val)
-	elseif data == 'string' then
-		return '"' .. val .. '"'
-	elseif data == 'Instance' then
-		return val:GetFullName()
-	else
-		return data .. '.new(' .. tostring(val) .. ')'
-	end
+    if not mt then
+        return tostring(v)
+    end
+
+    tostring_meta = mt.__tostring
+    setreadonly(mt, false)
+
+    mt.__tostring = nil
+    addr = tostring(v)
+
+    mt.__tostring = tostring_meta
+    setreadonly(mt, true)
+
+    return addr
 end
 
-local function serialize_table(t, p, c)
-    local s, counter = count_table(t), 1
-	local c = c or {}
-	local p = p or 1
-	local r = s > 0
-	local b = ''
+local function formatValue(val)
+    local data, ret = type(val)
 
-	local function format(v, d)
-		if d == 'table' then
-			return serialize_table(v, p + 1, c)
-		end
+    if compare(data, 'number', 'boolean', 'function', 'thread', 'nil') then
+        ret = tostring(val)
+    elseif compare(data, 'userdata', 'table') then
+        ret = stringRet(val)
+    elseif data == 'string' then
+        ret = '"' .. val .. '"'
+    elseif data == 'Instance' then
+        ret = val:GetFullName()
+    else
+        ret = data .. '.new(' .. tostring(val) .. ')'
+    end
 
-		return format_value(v)
-	end
-
-	for i, v in next, t do
-		local d, e = type(v), c[v]
-
-		if d == 'table' and (e and e[2] < p and e[1] == v or v == t) and p ~= 1 then
-			return format_value(e[1])
-		end
-
-		c[v] = {v, p}
-		b = b .. string.rep('  ', p) .. ('[' .. format(i, type(i)) .. '] = ') .. format(v, d) .. (s ~= counter and ',\n' or '\n')
-		counter = counter + 1
-	end
-
-	return '{' .. (r and '\n' or '') .. b .. string.rep('  ', (p and r) and p-1 or 0) ..'}'
+    return ret
 end
 
-return serialize_table
+local function serializeTable(t, p, c)
+    local s, counter = countTable(t), 1
+    local c = c or {}
+    local p = p or 1
+    local r = s > 0
+    local b = ''
+
+    local function format(v, d)
+        if d == 'table' then
+            return serializeTable(v, p + 1, c)
+        end
+
+        return formatValue(v)
+    end
+
+    for i, v in next, t do
+        local d, e = type(v), c[v]
+
+        if d == 'table' and (e and e[2] < p and e[1] == v or v == t) and p ~= 1 then
+            return formatValue(e[1])
+        end
+
+        c[v] = {v, p}
+        b = b .. string.rep('  ', p) .. ('[' .. format(i, type(i)) .. '] = ') .. format(v, d) .. (s ~= counter and ',\n' or '\n')
+        counter = counter + 1
+    end
+
+    return '{' .. (r and '\n' or '') .. b .. string.rep('  ', (p and r) and p-1 or 0) ..'}'
+end
+
+return serializeTable
