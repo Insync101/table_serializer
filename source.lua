590cd7576d96aa594e77471456d6b353030b0c7e
Corewave <45573459+Corewave@users.noreply.github.com>
2020-09-01 01:24:09 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,89 +1,86 @@
-type = (typeof or type)
-local function compare(o, ...)
-    for i, v in next, {...} do
-        if o == v then
-            return true
-        end
-    end
+type = typeof or type
+local str_types = {
+    ['boolean'] = true,
+    ['table'] = true,
+    ['userdata'] = true,
+    ['table'] = true,
+    ['function'] = true,
+    ['number'] = true,
+    ['nil'] = true
+}
 
-    return false
+local rawequal = rawequal or function(a, b)
+    return a == b
 end
 
-local function countTable(t)
-    local counter = 0
-    for i,v in next, t do
-        counter = counter + 1
+local function count_table(t)
+    local c = 0
+    for i, v in next, t do
+        c = c + 1
     end
 
-    return counter
+    return c
 end
 
-local function stringRet(v)
-    local mt = (getrawmetatable or getmetatable)(v)
-    local tostring_meta, addr
-
-    if not mt then
-        return tostring(v)
+local function string_ret(o, typ)
+    local ret, mt, old_func
+    if not (typ == 'table' or type == 'userdata') then
+        return tostring(o)
+    end
+    mt = (getrawmetatable or getmetatable)(o)
+    if not mt then 
+        return tostring(o)
     end
 
-    tostring_meta = mt.__tostring
+    old_func = rawget(mt, '__tostring')
     setreadonly(mt, false)
 
-    mt.__tostring = nil
-    addr = tostring(v)
-
-    mt.__tostring = tostring_meta
+    rawset(mt, '__tostring', nil)
+    ret = tostring(o)
+    rawset(mt, '__tostring', old_func)
     setreadonly(mt, true)
-
-    return addr
-end
-
-local function formatValue(val)
-    local data, ret = type(val)
-
-    if compare(data, 'number', 'boolean', 'function', 'thread', 'nil') then
-        ret = tostring(val)
-    elseif compare(data, 'userdata', 'table') then
-        ret = stringRet(val)
-    elseif data == 'string' then
-        ret = '"' .. val .. '"'
-    elseif data == 'Instance' then
-        ret = val:GetFullName()
-    else
-        ret = data .. '.new(' .. tostring(val) .. ')'
-    end
-
     return ret
 end
 
-local function serializeTable(t, p, c)
-    local s, counter = countTable(t), 1
-    local c = c or {}
-    local p = p or 1
-    local r = s > 0
-    local b = ''
+local function format_value(v)
+    local typ = type(v)
 
-    local function format(v, d)
-        if d == 'table' then
-            return serializeTable(v, p + 1, c)
-        end
+    if str_types[typ] then
+        return string_ret(v, typ)
+    elseif typ == 'string' then
+        return '"'..v..'"'
+    elseif typ == 'Instance' then
+        return v:GetFullName()
+    else
+        return typ..'.new(' .. tostring(v) .. ')'
+    end
+end
 
-        return formatValue(v)
+local function serialize_table(t, p, c, s)
+    local str = ""
+    local n = count_table(t)
+    local e = n > 0
+
+    c = c or {}
+    p = p or 1
+    s = s or string.rep
+
+    local function is_recursive(v, e)
+        return (e[2] < p and e[1] == v or rawequal and rawequal(v, t)) and p ~= 1
+    end
+
+    local function localized_format(v, typ)
+        return (typ == 'table' and not is_recursive(v, c[v])) and serialize_table(v, p + 1, c) or format_value(v)
     end
 
     for i, v in next, t do
-        local d, e = type(v), c[v]
+        local typ_i, typ_v = type(i), type(v)
+        c[i], c[v] = typ_i == 'table' and {i, p}, typ_v == 'table' and {v, p}
 
-        if d == 'table' and (e and e[2] < p and e[1] == v or v == t) and p ~= 1 then
-            return formatValue(e[1])
-        end
-
-        c[v] = {v, p}
-        b = b .. string.rep('  ', p) .. ('[' .. format(i, type(i)) .. '] = ') .. format(v, d) .. (s ~= counter and ',\n' or '\n')
-        counter = counter + 1
+        str = str .. s('  ', p) .. '[' .. localized_format(i, typ_i) .. ']' .. ' = ' .. localized_format(v, typ_v) .. '\n'
     end
 
-    return '{' .. (r and '\n' or '') .. b .. string.rep('  ', (p and r) and p-1 or 0) ..'}'
+    return ('{' .. (e and '\n' or '')) .. str .. (e and s('  ', p - 1) or '') .. '}'
 end
 
-return serializeTable
+return serialize_table
